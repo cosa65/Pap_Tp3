31170a9769232168cccaa8290512f8e8eb10ffe4
Karamchi <karamchi.ghandi@gmail.com>
2016-11-04 09:37:47 -0300

    P entregar

--- ej3/ej3.cpp
+++ ej3/ej3.cpp
@@ -6,11 +6,11 @@ using namespace std;
 int main() {
 	int c, a, q, act;
 	cin >> c >> a >> q;
-	vector<vector<int> > M(c + 1, vector<int>(a + 1, 0));
+	vector<vector<int> > S(c + 1, vector<int>(a + 1, 0));
 	for (int i = 1; i < c + 1; i++) {
 		for (int j = 1; j < a + 1; j++) {
 			cin >> act;
-			M[i][j] = M[i][j-1] + M[i-1][j] - M[i-1][j-1] + act;
+			S[i][j] = S[i][j-1] + S[i-1][j] - S[i-1][j-1] + act;
 		}
 	}
 
@@ -18,8 +18,8 @@ int main() {
 	for (int i = 0; i < q; i++) {
 		cin >> qc1 >> qa1 >> qc2 >> qa2;
 		//sumar desde lo que tiene +1 hasta lo que aspira
-		//las líneas de la matriz empiezan desde 1, parece a propósito
-		cout << M[qc2][qa2] - M[qc2][qa1] - M[qc1][qa2] + M[qc1][qa1] << endl;
+		//las líneas de la matriz empiezan desde 1
+		cout << S[qc2][qa2] - S[qc2][qa1] - S[qc1][qa2] + S[qc1][qa1] << endl;
 	}
 }
 
